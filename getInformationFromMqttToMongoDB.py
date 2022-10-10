d0cb1230c76910e74d39658a2478571ecf59bab8
dancingalfred <robban021@hotmail.com>
2022-10-10 11:38:56 +0200

    added addToDatabaseAndRemoveFromList function

--- getInformationFromMqttToMongoDB.py
+++ getInformationFromMqttToMongoDB.py
@@ -2,15 +2,20 @@ import paho.mqtt.client as mqtt #import the client
 import time
 #from functions import 
 
+sensorDataList =[]
+
 def on_message(client, userdata, message):
     print(message.topic+" "+str(message.payload))
     print("message received " ,str(message.payload.decode("utf-8")))
+    x = (message.topic+" "+str(message.payload))
+    sensorDataList.append(x)
 
 
-def returnPayload(client, userdata, message):
-    print(message.topic+" "+str(message.payload)) #("utf-8")
-    x = str(message.topic+" "+str(message.payload))
-    return x
+def addToDatabaseAndThenRemoveFromList(list:list):
+        if len(list) >= 1:
+            print("------sensorData-----------", list[0])
+            list.remove(list[0])
+
     
 #  NNSXS.JLAUBPS72BGVVQ4EY2Y6NRWPXAXC6YTBWOWELGA.2WD3PPT7IC4X63WXUVETFIV2GDLVJ4HT4LH25TXXFAKLD5QUADXQ
 #  eu1.cloud.thethings.network:1883
@@ -27,10 +32,9 @@ while True:
     client.subscribe("testtopic/4")
     print("Publishing message to topic","testtopic/4")
     client.publish("testtopic/4","OFF")
-    jjj = client.on_message
-    print("-----------------", jjj)
+    addToDatabaseAndThenRemoveFromList(sensorDataList)
 
-    time.sleep(5) # wait
+    time.sleep(8) # wait
     #client.loop_stop() #stop the loop
 
 
